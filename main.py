259cb5a3f8df4cc28167a0f919d6e5e47d13f60e
Manjunath2002 <manjunathlingamallu.com>
2021-11-08 16:10:12 +0530

    gitswork

---
+++ main.py
@@ -0,0 +1,13 @@
+num=int(input("enter the number"))
+
+s=1
+
+while(num!=0):
+    r=num%10
+    s=s*10+r
+    num=num/10
+if(num==s):
+    print("it is palindrome number")
+else:
+    print("it is not palindrome number")
+  
---
+++ main1.py
@@ -0,0 +1,7 @@
+num=int(input("enter the number:"))
+a=(num**2)
+if((a**0.5)==num):
+  print("it is a perfect square")
+else:
+  print("it is not a perfect square")
+ 
